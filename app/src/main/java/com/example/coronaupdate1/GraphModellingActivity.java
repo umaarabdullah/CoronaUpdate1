5b6e1234ff056005e5b7216e460e5d55859ade1f
Unknowncoder123 <umaar71@gmail.com>
2021-06-23 08:15:59 +0600

    Added the anyChart library and tested the column chart graph by passing some sample data.

--- app/src/main/java/com/example/coronaupdate1/GraphModellingActivity.java
+++ app/src/main/java/com/example/coronaupdate1/GraphModellingActivity.java
@@ -8,6 +8,24 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class GraphModellingActivity extends AppCompatActivity {
 
     private static final String TAG = "GraphModellingActivity";
@@ -45,6 +63,51 @@ public class GraphModellingActivity extends AppCompatActivity {
             newRecovered = getIntent().getStringExtra("new_recovered");
             totalTests = getIntent().getStringExtra("total_tests");
             Log.d(TAG, "onCreate: intent recieved " + countryName);
+
+
+
+            // getting the current date
+            Date date = Calendar.getInstance().getTime();
+            Log.d(TAG, "onCreate: current time -> " + date);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
+            // date is formatted as Ex : 28-Dec-2020
+            String formattedDate = simpleDateFormat.format(date);
+
+            AnyChartView anyChartView = findViewById(R.id.any_chart_view);
+            anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+
+            Cartesian cartesian = AnyChart.column();
+
+            List<DataEntry> casesData = new ArrayList<>();
+            casesData.add(new ValueDataEntry(formattedDate, 80540));
+            casesData.add(new ValueDataEntry("24/6/21", 70540));
+            casesData.add(new ValueDataEntry("25/6/21", 70540));
+            casesData.add(new ValueDataEntry("26/6/21", 8923));
+
+            Column column = cartesian.column(casesData);
+
+            column.tooltip()
+                    .titleFormat("{X}")
+                    .position(Position.CENTER_BOTTOM)
+                    .anchor(Anchor.CENTER_BOTTOM)
+                    .offsetX(0d)
+                    .offsetY(5d)
+                    .format("{%Value}{groupsSeparator: }");
+
+            cartesian.animation(true);
+            cartesian.title("Daily New Cases");
+
+            cartesian.yScale().minimum(0d);
+
+            cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
+
+            cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+            cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+            cartesian.xAxis(0).title("Date");
+            cartesian.yAxis(0).title("Cases");
+
+            anyChartView.setChart(cartesian);
 
         }
 
