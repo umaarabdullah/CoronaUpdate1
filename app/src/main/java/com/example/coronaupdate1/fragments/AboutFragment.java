143d269a87fc884f0c9abe134234843bc967fe6b
Umaar <umaar71@gmail.com>
2021-07-05 08:43:48 +0600

    Added google sign in authentication

--- app/src/main/java/com/example/coronaupdate1/fragments/AboutFragment.java
+++ app/src/main/java/com/example/coronaupdate1/fragments/AboutFragment.java
@@ -1,26 +1,51 @@
 package com.example.coronaupdate1.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.coronaupdate1.GoogleSignInActivity;
 import com.example.coronaupdate1.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AboutFragment extends Fragment {
+
+    private FirebaseAuth firebaseAuth;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
     @Override
     public View onCreateView(@NonNull @org.jetbrains.annotations.NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
 
-        // Inflate the view for this layout
-        // it returns a view type object
-        return inflater.inflate(R.layout.fragment_about, null);
+        View view = inflater.inflate(R.layout.fragment_about, null);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Button logOutButton = view.findViewById(R.id.log_out_button);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                Toast.makeText(getContext(), "Logged Out", Toast.LENGTH_SHORT).show();
+                startLogInPage();
+            }
+        });
+
+        return view;
     }
+
+    private void startLogInPage(){
+
+        startActivity(new Intent(getContext(), GoogleSignInActivity.class));
+    }
+
 }
